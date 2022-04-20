4dc8f7be95e2d357480af2889132f3b2518d2133
Nicolau Matos <nicolau.matos07@gmail.com>
2022-04-20 19:13:59 -0300

    adicionei o calculo de quanto cada um vai ter que pagar quando a gente for rachar a gasolina da viagem de sexta

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ console.log("Meu repositório no Git");
 console.log("Meu repositório no Git 2");
 console.log("Testando nomeação da branch");
 console.log("Pra ver como vai chegar no github")
+let km = 38
+let ida = 2
+let gasolina = km * ida / 11
+let valor = gasolina * 7 / 3
+console.log(valor)
